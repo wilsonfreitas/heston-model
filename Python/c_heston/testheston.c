74346bced5349f109e4601d6ef2d4e6724515403
wilsonfreitas <wilson.freitas@gmail.com>
2013-11-28 22:31:38 -0200

    Cleaning

--- Python/c_heston/testheston.c
+++ Python/c_heston/testheston.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 int main() {
-	double h = heston_u_call(100, 100, 0.5, 0.2, 0.1, 2, 0.2, -0.5);
+	double h = heston_ucall(100, 100, 0.5, 0.2, 0.1, 2, 0.2, -0.5);
 	printf("heston = %.20f\n", h);
 	return 0;
 }
